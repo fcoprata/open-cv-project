9c821c59300a524bb1ab8a4507c9b9838580e5f3
Francisco Prata <fcopratan@gmail.com>
2024-04-15 08:16:06 -0300

    Refactor object detection code in main.py

--- main.py
+++ main.py
@@ -1,80 +1,65 @@
 from ultralytics import YOLO
 import cv2
-import math
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+# set webcam resolution
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 
-def detect_objects():
-    """
-    Detects and displays objects in real-time using YOLO object detection model.
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
 
-    This function starts the webcam, loads the YOLO model, and continuously captures frames from the webcam.
-    It detects objects in each frame using the YOLO model and displays the frame with bounding boxes and labels
-    around the detected objects.
+    # coordinates
+    for r in results:
+        boxes = r.boxes
 
-    Returns:
-        None
-    """
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-    # Start webcam
-    cap = cv2.VideoCapture(0)
-    cap.set(3, 640)
-    cap.set(4, 480)
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    # Load YOLO model
-    model = YOLO("yolo-Weights/yolov8n.pt")
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
 
-    # Object classes
-    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-                  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-                  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-                  "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-                  "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-                  "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-                  "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-                  "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-                  "teddy bear", "hair drier", "toothbrush"
-                  ]
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
 
-    while True:
-        success, img = cap.read()
-        results = model(img, stream=True)
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
 
-        # Process object detection results
-        for r in results:
-            boxes = r.boxes
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
-            for box in boxes:
-                # Bounding box coordinates
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # Convert to int values
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-                # Draw bounding box on the frame
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-                # Confidence of the detected object
-                confidence = math.ceil((box.conf[0] * 100)) / 100
-                print("Confidence --->", confidence)
-
-                # Class name of the detected object
-                cls = int(box.cls[0])
-                print("Class name -->", classNames[cls])
-
-                # Display object details on the frame
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
-        # Display the frame with detected objects
-        cv2.imshow('Webcam', img)
-
-        # Exit loop if 'q' is pressed
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    # Release the webcam and close all windows
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
